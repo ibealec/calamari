5f919a6758373db8499c5dd663c23b679be507d5
Alec Branaa <ibealec@gmail.com>
2020-04-10 21:23:14 -0700

    Save previous command

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# calamari
+# Calamari
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/ibealec/calamari)
+
+## Description
+
+This bite-sized package helps you get your microservices up and running quickly.
+
+Yes, I do know Docker is a thing and I know Kubernetes exists. 😊
+
+## Table of Contents
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [License](#license)
+
+- [Contributing](#contributing)
+
+- [Tests](#tests)
+
+- [Questions](#questions)
+
+## Installation
+
+To install calamari, run the following command:
+
+```
+npm i node-calamari -D
+```
+
+## Usage
+
+Run `calamari` in your command line
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+
+Want to contribute? Open an issue or PR!
+
+## Tests
+
+Someday...
+
+## Questions
+
+If you have any questions about the repo, open an issue or [contact me](alec@ossenx.com) directly.
---
+++ index.js
@@ -0,0 +1,124 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const chalk = require("chalk");
+const walk = require("walk");
+const { Confirm, MultiSelect, Select } = require("enquirer");
+const shell = require("shelljs");
+
+const oldLog = console.log;
+console.log = function (...args) {
+  return oldLog(chalk.green(args));
+};
+
+const folders = [];
+const files = [];
+
+const options = {
+  followLinks: false,
+  // directories with these keys will be skipped
+  filters: ["node_modules"],
+};
+
+function recursivelyFindServices() {
+  const walker = walk.walk(".", options);
+  console.log("Scanning folders for packages...");
+
+  walker.on("file", function (root, stat, next) {
+    if (stat.name === "package.json") {
+      folders.push(root);
+      console.log(root);
+      const dirPath = root;
+      const path = root + "/" + stat.name;
+      const fileData = JSON.parse(fs.readFileSync(path, "utf8"));
+      const name = fileData.name;
+      const startScripts = Object.keys(fileData.scripts).filter((name) => {
+        if (name.includes("start")) {
+          return name;
+        }
+      });
+      files.push({
+        name,
+        startScripts,
+        dirPath,
+        value: name,
+      });
+    }
+    next();
+  });
+
+  walker.on("end", function () {
+    const prompt = new MultiSelect({
+      name: "value",
+      message: "Which services do you want to start?",
+      limit: 7,
+      choices: [...files],
+    });
+
+    prompt
+      .run()
+      .then((answers) => {
+        selectedServices = files.filter(
+          (file) => answers.indexOf(file.name) !== -1
+        );
+        let i = 0;
+        askQuestion(i);
+      })
+      .catch(console.error);
+  });
+}
+
+let selectedServices = [];
+
+var askQuestion = function (i) {
+  if (i < selectedServices.length) {
+    const prompt = new Select({
+      name: "value",
+      message: `Which script do you want to run for`,
+      choices: [...selectedServices[i].startScripts],
+    });
+    i++;
+    return prompt.run().then(function (answer) {
+      selectedServices[i - 1].command = answer;
+      return askQuestion(i);
+    });
+  } else {
+    let commandsString = ``;
+    selectedServices.forEach((service) => {
+      commandsString += `"cd ${service.dirPath} && npm run ${service.command}" `;
+    });
+    fs.writeFileSync(
+      "previousCommands.json",
+      JSON.stringify({ mostRecent: commandsString })
+    );
+    shell.exec(`concurrently ${commandsString}`);
+  }
+};
+
+try {
+  if (fs.existsSync("./previousCommands.json")) {
+    const { mostRecent } = JSON.parse(
+      fs.readFileSync("./previousCommands.json")
+    );
+    const prompt = new Confirm({
+      name: "question",
+      message: "Want to go with your last config?",
+      initial: "y",
+    });
+
+    prompt
+      .run()
+      .then((useLastConfig) => {
+        if (useLastConfig) {
+          shell.exec(`concurrently ${mostRecent}`);
+        } else {
+          recursivelyFindServices();
+        }
+      })
+      .catch(console.error);
+  } else {
+    recursivelyFindServices();
+  }
+} catch (err) {
+  console.error(err);
+}
